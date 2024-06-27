39179d72c984259a996afcf9d097f35388e93568
Gabriel-Barria <ibarria302@gmail.com>
2024-06-27 17:37:14 -0400

    Se repara el retorno de delete

--- app/Lecturas/deleteLectura.php
+++ app/Lecturas/deleteLectura.php
@@ -1,8 +1,4 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
 header('Content-Type: application/json');
 
 require 'Lectura.php';
@@ -14,5 +10,5 @@ $idlectura = $_GET['idlectura'];
 
 $lectura->delete($idlectura);
 
-echo json_encode(["message" => "Lectura eliminada exitosamente"]);
+echo json_encode(['success' => true, 'message' => 'Lectura eliminada exitosamente']);
 ?>
